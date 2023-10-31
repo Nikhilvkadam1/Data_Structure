45b9ca1d4234926424f510938bef500520cd4355
Nikhil Kadam <143273224+Nikhilvkadam1@users.noreply.github.com>
2023-11-01 01:17:39 +0530

    Update dequeue.c

--- dequeue.c
+++ dequeue.c
@@ -1,125 +1,150 @@
 #include <stdio.h>
-#define Max_size 5
-int deque[];
-int front = -1;
-int rear = -1;
-int x;
-void enqueuefront(){
-    if((front == 0 && rear == Max_size-1) || (front==rear +1 )){
-        printf("queue is overflow");
-    }else{
-    printf("\nEnter Number : ");
-    scanf("%d",&x);
-    if(front==-1 && rear==-1){
-        front=rear=0;
-        deque[front]=x;
-    }else if(front == 0){
-       front = Max_size-1;
-       deque[front]=x;
-    }else{
-        front--;
-        deque[front]=x;
-      }
-    }
-}
-void enqueuerear(){
-    if((front == 0 && rear == Max_size-1) || (front==rear +1 )){
-        printf("queue is overflow");
-    }else{
-    printf("\nEnter Number : ");
-    scanf("%d",&x);
-    if(front==-1 && rear==-1){
-        front=rear=0;
-        deque[front]=x;
-    }else if(rear == Max_size-1){
-       rear =0;
-       deque[rear]=x;
-    }else{
-        rear--;
-        deque[rear]=x;
-      }
-    }
-}
-void dequeuefront(){
-    if(rear==-1 && front==-1){
-        printf("Stack is Underflow");
 
-    }else if(front==rear){
-        front=rear=-1;
-    }
-    else if(front == Max_size-1){
-        printf("%d deleted from deque.",deque[front]);
-        front = 0;
-    }else{
-        printf("%d deleted from deque.",deque[front]);
-        front++;
-    }
-}
-void dequeuerear(){
-    if(rear==-1 && front==-1){
-        printf("Stack is Underflow");
+#define MAX_SIZE 100
 
-    }else if(front==rear){
-        front=rear=-1;
-    }
-    else if(rear == 0){
-        printf("%d deleted from deque.",deque[front]);
-        rear = Max_size-1;
-    }else{
-        printf("%d deleted from deque.",deque[front]);
-        rear++;
-    }
+// Structure to represent a deque
+struct Deque {
+    int array[MAX_SIZE];
+    int front;
+    int rear;
+    int size;
+};
+
+// Function to initialize a deque
+void initializeDeque(struct Deque* deque) {
+    deque->front = -1;
+    deque->rear = 0;
+    deque->size = 0;
 }
-void display(){
-    int i= front;
-    while(i != rear){
-            printf("%d\t",deque[i]);
-            i=(i+1)%Max_size;
-        }
-        printf("%d\t",deque[i]);
-        printf("\n");
+
+// Function to check if the deque is empty
+int isEmpty(struct Deque* deque) {
+    return (deque->size == 0);
 }
-void main() {
-    int op,x;
-     printf("Menu\n1.enqueuefront\n2.enqueuerear\n3.dequeuefront\n4.dequeuefrontrear\n5.Display\n6.End");
-    do{
-        printf("\nChoose Option: ");
-        scanf("%d",&op);
-            switch(op){
-                case 1:
-                {
-                    enqueuefront();
-                    break;
+
+// Function to check if the deque is full
+int isFull(struct Deque* deque) {
+    return (deque->size == MAX_SIZE);
+}
+
+// Function to add an element to the front of the deque
+void pushFront(struct Deque* deque, int item) {
+    if (isFull(deque)) {
+        printf("Deque is full. Cannot push to front.\n");
+        return;
+    }
+    if (deque->front == -1) {
+        deque->front = 0;
+        deque->rear = 0;
+    } else if (deque->front == 0) {
+        deque->front = MAX_SIZE - 1;
+    } else {
+        deque->front--;
+    }
+    deque->array[deque->front] = item;
+    deque->size++;
+}
+
+// Function to add an element to the rear of the deque
+void pushRear(struct Deque* deque, int item) {
+    if (isFull(deque)) {
+        printf("Deque is full. Cannot push to rear.\n");
+        return;
+    }
+    if (deque->front == -1) {
+        deque->front = 0;
+        deque->rear = 0;
+    } else if (deque->rear == MAX_SIZE - 1) {
+        deque->rear = 0;
+    } else {
+        deque->rear++;
+    }
+    deque->array[deque->rear] = item;
+    deque->size++;
+}
+
+// Function to remove an element from the front of the deque
+int popFront(struct Deque* deque) {
+    if (isEmpty(deque)) {
+        printf("Deque is empty. Cannot pop from front.\n");
+        return -1; // Return a sentinel value for empty deque
+    }
+    int item = deque->array[deque->front];
+    if (deque->front == deque->rear) {
+        deque->front = -1;
+        deque->rear = 0;
+    } else if (deque->front == MAX_SIZE - 1) {
+        deque->front = 0;
+    } else {
+        deque->front++;
+    }
+    deque->size--;
+    return item;
+}
+
+// Function to remove an element from the rear of the deque
+int popRear(struct Deque* deque) {
+    if (isEmpty(deque)) {
+        printf("Deque is empty. Cannot pop from rear.\n");
+        return -1; // Return a sentinel value for empty deque
+    }
+    int item = deque->array[deque->rear];
+    if (deque->front == deque->rear) {
+        deque->front = -1;
+        deque->rear = 0;
+    } else if (deque->rear == 0) {
+        deque->rear = MAX_SIZE - 1;
+    } else {
+        deque->rear--;
+    }
+    deque->size--;
+    return item;
+}
+
+int main() {
+    struct Deque deque;
+    initializeDeque(&deque);
+
+    int choice, item;
+
+    while (1) {
+        printf("1. Push to front\n");
+        printf("2. Push to rear\n");
+        printf("3. Pop from front\n");
+        printf("4. Pop from rear\n");
+        printf("5. Quit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter an element to push to the front: ");
+                scanf("%d", &item);
+                pushFront(&deque, item);
+                break;
+            case 2:
+                printf("Enter an element to push to the rear: ");
+                scanf("%d", &item);
+                pushRear(&deque, item);
+                break;
+            case 3:
+                item = popFront(&deque);
+                if (item != -1) {
+                    printf("Popped from front: %d\n", item);
                 }
-                case 2:
-                {
-                    enqueuerear();
-                    break;
+                break;
+            case 4:
+                item = popRear(&deque);
+                if (item != -1) {
+                    printf("Popped from rear: %d\n", item);
                 }
-                case 3:
-                {
-                    dequeuefront();
-                    break;
-                }
-                case 4:
-                {
-                    dequeuerear();
-                    break;
-                }
-                case 5:
-                {
-                    display();
-                    break;
-                }
-                case 6:
-                {
-                    printf("End Point");
-                    break;
-                }
-                default :
-                {
-                    printf("Invalid option");
-                }
+                break;
+            case 5:
+                return 0;
+            default:
+                printf("Invalid choice. Try again.\n");
         }
-    }while(op!=6);
+    }
+
+    return 0;
 }
